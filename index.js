8a7faa26658eaa5f5fe78f343d37ffb917120133
Brian Donohue <brian.donohue89@gmail.com>
2015-09-19 13:59:55 -0400

    Implemented Venmo pay module
    Signed-off-by: Brian Donohue <brian.donohue89@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var rest = require('restler');
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +7,46 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var username = step.input('username').first();
+        var amount = step.input('amount').first();
+        var note = step.input('note').first();
+        var access_token = dexter.environment('venmo_access_token');
+
+        if (username == undefined || !username.length) {
+            return this.fail({'message': 'Invalid username'});
+        }
+        else if (amount == undefined || isNaN(amount)) {
+            this.log(amount);
+            return this.fail({'message': 'Invalid amount'});
+        }
+        else if (note == undefined || !note.length) {
+            return this.fail({'message': 'Invalid note'});
+        }
+        else if (access_token == undefined || !access_token.length) {
+            return this.fail({'message': 'Invalid access token'});
+        }
+
+        var self = this;
+        var postData = {
+            data: {
+                username: step.input('username').first(),
+                amount: step.input('amount').first(),
+                note: step.input('note').first(),
+                access_token: dexter.environment('venmo_access_token')
+            }
+        };
+
+        rest.post('https://api.venmo.com/v1/payments', postData).on('complete', function(result, response) {
+            if (response.statusCode != 200) {
+                return self.fail({
+                    statusCode: response.statusCode,
+                    headers: response.headers,
+                    data: result,
+                    message: 'Payment failed. Invalid status code: ' + response.statusCode
+                });
+            }
+
+            self.complete({});
+        });
     }
 };
